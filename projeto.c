1112829f38996355dbd7d5ee397729fb53cdb81e
Pedro-costa99 <53200356+Pedro-costa99@users.noreply.github.com>
2021-07-06 13:06:50 -0300

    Implementação - Resoluções a, b, c, d, e, f, g

--- projeto.c
+++ projeto.c
@@ -5,12 +5,13 @@
 IFBA
 Disciplina: INF006 - Estrutura de dados
 Docente: Prof. Antônio Carlos
-Discente: Pedro Fernando C. Costa
-Semestre: 2021.
+Discente: Pedro Costa
+Semestre: 2021.1
 Atividade: Mercado simulado de compra e venda de ações
 Entrega parcial: 21/06/2021/ Resolução das letras a e b.
 */
 
+//declaração de struct
 typedef struct ponto
 {
 	int quantidade;
@@ -25,27 +26,48 @@ typedef struct ponto
 
 int main(int argc, char const *argv[])
 {
+	FILE *pont_arq;//cria variável ponteiro para o arquivo
+
+	//abrindo o arquivo com tipo de abertura w
+	pont_arq = fopen("arquivo_acoes.txt", "w");
+
+	//testando a criação do arquivo
+	if(pont_arq == NULL){
+		printf("Erro na abertura do arquivo\n");
+		return 1;
+	} else{
+
+
 	t_ponto* ini_ponto;//início da lista
 	t_ponto* proximo_ponto;
 	int resp;
-	int sorte01, sorte02;
+	int sorte01, sorte02, atual = 0, anterior = 0, codigo = 0, codigo03 = 0, atual02 =0, anterior02 =0;
 
+	//alocação de memória
 	ini_ponto = (t_ponto *)malloc(sizeof(t_ponto));
 	if(ini_ponto == NULL)
 		exit(1);
 
+	//aponta para o mesmo endereço de ini_ponto
 	proximo_ponto = ini_ponto;
 
 	while(1){
 		printf("Digite a quantidade: \n");
 		scanf("%d", &proximo_ponto->quantidade);
+		//usando fprintf para armazenar a string no arquivo
+		fprintf(pont_arq, "%d\n", proximo_ponto->quantidade);
 
 		printf("Digite o valor: \n");
 		scanf("%f", &proximo_ponto->valor);
+		//usando fprintf para armazenar a string no arquivo
+		fprintf(pont_arq, "%d\n", proximo_ponto->valor);
 
 		do{
 			printf("Qual o tipo? <2> COMPRA <3> VENDA: \n");
 			scanf("%d", &sorte01);
+			//usando fprintf para armazenar a string no arquivo
+			fprintf(pont_arq, "%d\n", sorte01);
+
 
 			proximo_ponto->tipo = sorte01;
 
@@ -55,6 +77,8 @@ int main(int argc, char const *argv[])
 		do{
 			printf("Digite a sigla: <4> PETR4 <5> VALE5 <6> ITSA4 <7> USIM5 <8> LAME3 \n");
 			scanf("%d", &sorte02); 
+			//usando fprintf para armazenar a string no arquivo
+			fprintf(pont_arq, "%d\n", sorte02);
 
 			proximo_ponto->sigla = sorte02;
 
@@ -77,6 +101,41 @@ int main(int argc, char const *argv[])
 
 		while(proximo_ponto != NULL){
 			printf("Quantidade: %d, Valor: %.2f, ", proximo_ponto->quantidade, proximo_ponto->valor);
+
+			//verificando igualdade de valores
+			if(codigo == 0){
+				atual = proximo_ponto->valor;
+				
+			}
+
+			int codigo02 = 0;
+			if(codigo02 == 0 && codigo > 0){
+				anterior = proximo_ponto->valor;				
+			}
+
+			codigo++;
+			
+
+			if(proximo_ponto->tipo == 2){
+				printf("Tipo: Compra, ");
+			}else{
+				printf("Tipo: Venda, ");
+			}
+
+			//comparando a quantidade
+			if(codigo03 == 0){
+				atual02 = proximo_ponto->quantidade;
+				
+			}
+
+			int codigo04 = 0;
+			if(codigo04 == 0 && codigo03 > 0){
+				anterior02 = proximo_ponto->quantidade;				
+			}
+
+			codigo03++;
+			
+
 			if(proximo_ponto->tipo == 2){
 				printf("Tipo: Compra, ");
 			}else{
@@ -96,9 +155,35 @@ int main(int argc, char const *argv[])
 			}else if(proximo_ponto->sigla == 8){
 				printf("Sigla: LAME3\n");
 			}
+
 			
 			proximo_ponto = proximo_ponto->proximo;
+
+			if (anterior != atual && anterior02 == atual02 && proximo_ponto == NULL)
+			{
+				printf("\n");				
+				printf("Desculpa, a operacao nao pode ser realizada. \n");
+				printf("ValorVenda deve ser igual ao de ValorCompra\n");
+				printf("Quantidade de venda ou compra deve ser menor das duas quanitdades\n");
+			} else if (anterior == atual && anterior02 == atual02 && proximo_ponto == NULL){
+			
+				printf("\n");				
+				printf("Desculpa, a operacao nao pode ser realizada. \n");				
+				printf("Quantidade de venda ou compra deve ser menor das duas quantidades\n");
+			}else if (anterior == atual && anterior02 != atual02 && proximo_ponto == NULL){
+				printf("\n");
+				printf("Operacao realizada com sucesso!!!\n");
+			}
+			
 		}
+
+		fclose(pont_arq);
+
+
+	}
+
+
+
 	
 	return 0;
 }
